c779af759701d6ab7992e44fb659e985e4c98d1f
Virajjj646 <viraj.deglurkar@gmail.com>
2026-03-03 16:44:21 +0530

    UC5-Stack

---
+++ APP/src/UC5PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class UC5PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop and compare
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
